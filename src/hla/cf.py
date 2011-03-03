7823206f154c05589b8e51f374daad5bb3033d3f
Lingyun Yang <lyyang@bnl.gov>
2011-03-03 18:14:45 -0500

    updated conf/cf module, obsolete importing xml to conf

--- src/hla/cf.py
+++ src/hla/cf.py
@@ -8,8 +8,9 @@ This module builds a local cache of channel finder service.
 """
 
 import cadict
-import shelve
+import re, shelve
 from time import gmtime, strftime
+import sys
 
 class ChannelFinderAgent:
     def __init__(self):
@@ -18,6 +19,8 @@ class ChannelFinderAgent:
         self.__elempv = {}
 
     def importXml(self, fname):
+        """OBSOLETE - the main.xml file does not have latest pv list
+        """
         ca = cadict.CADict(fname)
         for elem in ca.elements:
             #print elem
@@ -25,6 +28,8 @@ class ChannelFinderAgent:
             if len(elem.ca) == 0: continue
             for i, pv in enumerate(elem.ca):
                 # for each pv and handle
+                #print "'%s'" % pv, 
+                if len(pv.strip()) == 0: continue
                 self.addChannel(pv, {'handle':elem.handle[i], 
                                      'elementname':elem.name,
                                      'elementtype':elem.type,
@@ -35,6 +40,60 @@ class ChannelFinderAgent:
 
             #print elem.sequence
         self.__cdate = strftime("%Y-%m-%dT%H:%M:%S", gmtime())
+
+    def __parseElementName(self, name):
+        # for NSLS-2 convention of element name
+        a = re.match(r'.+(G\d{1,2})(C\d{1,2})(.)', name)
+        if a:
+            girder   = a.groups()[0]
+            cell     = a.groups()[1]
+            symmetry = a.groups()[2]
+        elif name == "CAVITY":
+            # fix a broken name
+            girder   = "CAVITY"
+            cell     = "CAVITY"
+            symmetry = "CAVITY"
+        else:
+            girder   = 'G0'
+            cell     = 'C00'
+            symmetry = '0'
+        return cell, girder, symmetry
+
+    def importLatticeTable(self, lattable):
+        """Import the table used for Tracy-VirtualIOC
+
+        #index, read back, set point, phys name, len[m], s[m], group name(type)
+
+        Data are deliminated by spaces
+        """
+
+        print "Importing file:", lattable
+
+        cnt = {'BPM':0, 'TRIMD':0, 'TRIMX':0, 'TRIMY':0, 'SEXT':0, 'QUAD':0}
+
+        f = open(lattable, 'r').readlines()
+        d = []
+        for s in f[1:]:
+            t = s.split()
+            d.append([int(t[0]), t[1].strip(), t[2].strip(), t[3].strip()])
+            d[-1].extend([float(t[i]) for i in range(4, 6)])
+            d[-1].append(t[6].strip())
+            #print d[-1]
+            k = 6
+            if cnt.has_key(d[-1][k]): cnt[d[-1][k]] += 1
+            # parse cell/girder/symmetry from name
+            cell, girder, symmetry = self.__parseElementName(d[-1][3])
+            # add the readback pv
+            self.addChannel(d[-1][1], {'handle': 'get', 
+                                     'elementname': d[-1][3],
+                                     'elementtype': d[-1][6],
+                                     'cell': cell,
+                                     'girder': girder,
+                                     'symmetry': symmetry}, None)
+        print "Summary:"
+        for k,v in cnt.items():
+            print " %8s %4d" % (k, v)
+        return d
 
     def save(self, fname):
         f = shelve.open(fname, 'c')
@@ -108,10 +167,37 @@ class ChannelFinderAgent:
             if pvs: ret[elem] = pvs
         return ret
 
+    def testChannels(self):
+        import cothread
+        from cothread.catools import caget
+
+        rec = {}
+        n = 0
+        print "Testing %d channels" % len(self.__d.keys())
+        for v in sorted(self.__d.keys()):
+            #print "'%s'" % v, 
+            n += 1
+            try:
+                x = caget(v, timeout=2)
+                sys.stdout.write('-')
+            except:
+                #print "TIME OUT"
+                rec[v] = 1
+                sys.stdout.write('\nx %6d  %s' % (n,v))
+                
+            sys.stdout.flush()
+        print ""
+        for k,v in rec.items():
+            print k, v
+        
 
 if __name__ == "__main__":
     d = ChannelFinderAgent()
-    d.importXml('/home/lyyang/devel/nsls2-hla/machine/nsls2/main.xml')
+    #d.importXml('/home/lyyang/devel/nsls2-hla/machine/nsls2/main.xml')
+    d.importLatticeTable('/home/lyyang/devel/nsls2-hla/machine/nsls2/lat_conf_table.txt')
+    d.testChannels()
+    #print d
+    sys.exit(0)
 
     #print d
     d.save('test.shelve')
