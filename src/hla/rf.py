33c3d75181bbd1510e513e1521e83cf4e88ec4f0
Lingyun Yang <lyyang@bnl.gov>
2011-03-14 16:39:04 -0400

    add rf module

---
+++ src/hla/rf.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+"""
+    hla.rf
+    ~~~~~~
+
+    RF related
+
+    :author: Lingyun Yang
+    :license: (empty ? GPL ? EPICS ?)
+
+"""
+
+from cothread.catools import caget, caput
+
+def getRfFrequency():
+    """Get the RF frequency"""
+    return caget("SR:C00-RF:G00<RF:00>Freq-RB")
+
+def putRfFrequency(f):
+    """set the rf frequency"""
+    caput("SR:C00-RF:G00<RF:00>Freq-SP", f)
+
+def getRfVoltage():
+    """Return RF voltage"""
+    return caget("SR:C00-RF:G00<RF:00>Volt-RB")
+
+
+
