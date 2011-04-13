206c1f151a76388810f044c152a694670d2bbf1f
Lingyun Yang <lyyang@bnl.gov>
2011-04-13 18:09:24 -0400

    remove main.xml, update orbit test

--- test/utOrbit.py
+++ test/utOrbit.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 
+from conf import *
+
 import hla
 import unittest
 import sys, os
 import numpy as np
 import random
 
-from conf import *
 
 class TestOrbit(unittest.TestCase):
     """
@@ -22,7 +23,7 @@ class TestOrbit(unittest.TestCase):
         self.orbit = hla.orbit.Orbit(hla._cfa)
         pass
 
-    def test_orbit():
+    def test_orbit(self):
         self.assertTrue(len(self.orbit.get()) > 0)
 
 if __name__ == "__main__":
