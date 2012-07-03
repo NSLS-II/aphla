231e1746fabee49347776a0d61ad8b6815af0605
Lingyun Yang <lyyang@bnl.gov>
2012-07-03 01:20:57 -0400

    add compare script

---
+++ utils/compareChanFinder.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+import sys
+
+from channelfinder import ChannelFinderClient, Channel, Property, Tag
+import aphla as ap
+
+cfsurl = 'http://web01.nsls2.bnl.gov:8080/ChannelFinder'
+#cfinput = {
+#    'BaseURL': cfsurl,
+#    'username': conf.username,
+#   'password': conf.password}
+
+def download_cfs(cfsurl, **cfinput):
+    cf = ChannelFinderClient(BaseURL = cfsurl)
+    chs = cf.find(**cfinput)
+    ret = chanData.ChannelFinderData()
+    for ch in chs:
+        pv = ch.Name
+        prpt = dict([(p.Name, p.Value) for p in ch.Properties])
+        tags = [t.Name for t in ch.Tags]
+
+        ret.update(pv = pv, properties = prpt, tags = tags)
+    return ret
+    
+
+def print_kv(k, v):
+    if len(v[0]) == 0 and len(v[1]) == 0: return
+    elif len(v[1]) == 0:
+        print k, 
+        for p,vv in v[0].iteritems():
+            print "%s=%s" % (str(p), str(vv)),
+        print ""
+    elif len(v[0]) == 0:
+        print k, 
+        for p in v[1]:
+            print "%s," % p,
+        print ""
+    else:
+        print k,
+        for p,vv in v[0].iteritems():
+            print "%s=%s" % (str(p), str(vv)),
+        for p in v[1]:
+            print "%s," % p
+        print ""
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print sys.argv[0], "us_nsls2_cfs.csv"
+        sys.exit(0)
+
+    cfa1 = ap.ChannelFinderAgent()
+    cfa1.importCsv(sys.argv[1])
+
+    cfa2 = ap.ChannelFinderAgent()
+    cfa2.downloadCfs(cfsurl, tagName='aphla.*')
+
+    print "<<<< local - CFS"
+    for k, v in (cfa1 - cfa2).iteritems():
+        print_kv(k, v)
+    print "--------------"
+    for k, v in (cfa2 - cfa1).iteritems():
+        print_kv(k, v)
+    print ">>>> CFS - local"
+
